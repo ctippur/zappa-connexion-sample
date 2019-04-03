91a32512585435d5ab7392c063c7811cf8daaf1d
Shekar Tippur <shekartippur@Shekars-MacBook-Pro.local>
2019-04-03 01:36:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# zappa-connexion-sample
+
+
+# Create a virtual env
+python3 -m venv --without-pip myvenv
+source myvenv/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py | python
+deactivate
+source myvenv/bin/activate
+pip install -r requirements.txt
+
+# Test server locally
+python -m swagger_server.app
+curl -X GET "http://0.0.0.0:5000/" -H "accept: text/plain"
+
+# Deploy to zappa
+zappa deploy dev
+
+# Test zappa
+curl -X GET "https://um8wz2hhyh.execute-api.us-east-1.amazonaws.com/dev/" -H "accept: text/plain"
---
+++ swagger_server/app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+'''
+Basic example of a resource server
+'''
+
+import time
+import sys,os
+sys.path.append(os.getcwd() + '/swagger_server')
+
+import connexion
+import six
+from werkzeug.exceptions import Unauthorized
+
+from jose import JWTError, jwt
+
+JWT_ISSUER = 'com.zalando.connexion'
+JWT_SECRET = 'change_this'
+JWT_LIFETIME_SECONDS = 600
+JWT_ALGORITHM = 'HS256'
+
+
+def generate_token(user_id):
+    timestamp = _current_timestamp()
+    payload = {
+        "iss": JWT_ISSUER,
+        "iat": int(timestamp),
+        "exp": int(timestamp + JWT_LIFETIME_SECONDS),
+        "sub": str(user_id),
+    }
+
+    return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+
+def decode_token(token):
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except JWTError as e:
+        six.raise_from(Unauthorized, e)
+
+
+def get_secret(user, token_info) -> str:
+    return '''
+    You are user_id {user} and the secret is 'wbevuec'.
+    Decoded token claims: {token_info}.
+    '''.format(user=user, token_info=token_info)
+
+
+def _current_timestamp() -> int:
+    return int(time.time())
+
+def say_hello() -> int:
+    return int(time.time())
+
+app = connexion.FlaskApp(__name__, specification_dir='swagger/')
+if __name__ == '__main__':
+    app.add_api('openapi.yaml')
+    app.run()
+
